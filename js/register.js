ee78578336cb6dcd84fcddb978533bf66c587c05
Gabriel <gabrielzrwestfal@gmail.com>
2026-06-24 09:43:53 -0300

    inicio formulario local storage

---
+++ js/register.js
@@ -0,0 +1,8 @@
+const inNome = document.getElementById("inNome");
+const inCpf = document.getElementById("inCpf");
+const inEmail = document.getElementById("inEmail");
+const inTelefone = document.getElementById("inTelefone");
+const inNascimento = document.getElementById("inNascimento");
+const inSenha = document.getElementById("inSenha");
+
+
