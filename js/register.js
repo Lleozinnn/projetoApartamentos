29afe403e71a81910a54f3fe3e87f8bb1381dff3
Gabriel <gabrielzrwestfal@gmail.com>
2026-06-25 11:33:20 -0300

    iniciando pagina register

--- js/register.js
+++ js/register.js
@@ -12,8 +12,23 @@ const cheTermo3 = document.getElementById("termo3");
 const btnCadastrar = document.getElementById("btnCadastrar");
 
 // Escutar evento do botão de cadastro
-btnCadastrar.addEventListener("click",cadastrar());
-
-// Criar função que valida os campos do formulario 
+btnCadastrar.addEventListener("click", (event)=> cadastrar(event));
 
 // Criar função que efetua o cadastro no local storage
+function cadastrar(event){
+    event.preventDefault();
+    let cadastro = {
+        nome:inNome.value,
+        cpf:inCpf.value,
+        email:inEmail.value,
+        telefone:inTelefone.value,
+        nascimento:inNascimento.value,
+        senha:inSenha.value,
+        genero:rdGenero.value
+    };
+    if(cheTermo1.checked && cheTermo2.checked){
+        cadastro.termos = true;
+    }
+     cadastro.comunicacao = cheTermo3.checked ?  true : false;
+     console.log(cadastro);
+}
