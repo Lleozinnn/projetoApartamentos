e0ddadf212d590598e6e06025596279e00346f5f
Gabriel <gabrielzrwestfal@gmail.com>
2026-06-25 10:11:57 -0300

    alteração de nome do formulario

--- js/register.js
+++ js/register.js
@@ -1,8 +1,19 @@
+//referencias dos elementos do formulário
 const inNome = document.getElementById("inNome");
 const inCpf = document.getElementById("inCpf");
 const inEmail = document.getElementById("inEmail");
 const inTelefone = document.getElementById("inTelefone");
 const inNascimento = document.getElementById("inNascimento");
 const inSenha = document.getElementById("inSenha");
+const rdGenero = document.getElementById("genero");
+const cheTermo1 = document.getElementById("termo1");
+const cheTermo2 = document.getElementById("termo2");
+const cheTermo3 = document.getElementById("termo3");
+const btnCadastrar = document.getElementById("btnCadastrar");
 
+// Escutar evento do botão de cadastro
+btnCadastrar.addEventListener("click",cadastrar());
 
+// Criar função que valida os campos do formulario 
+
+// Criar função que efetua o cadastro no local storage
