fb14069053bc384cd63d8de688d8c1de5d7935e4
Gabriel <gabrielzrwestfal@gmail.com>
2026-06-29 11:27:02 -0300

    parte de cadastro, deletar e editar apartamentos finalizada

--- js/apartment.js
+++ js/apartment.js
@@ -1,3 +1,6 @@
+// Verificação de login 
+let sessao = JSON.parse(localStorage.getItem("sessao"));
+console.log(sessao);
 const inNumero = document.getElementById("inNumero");
 const rdCidade = document.getElementsByName("cidade");
 const inPreco = document.getElementById("inPreco");
@@ -8,9 +11,15 @@ const btCadastrar = document.getElementById("btCadastrar");
 const outSaida = document.getElementById("outSaida");
 const btDeletar = document.getElementById("btdeletar");
 const btEditar = document.getElementById("btEditar");
+const btLimpar = document.getElementById("btLimpar");
+
 btCadastrar.addEventListener("click", (event) => cadastrar(event));
 btDeletar.addEventListener("click", (event) => deletar(event));
 btEditar.addEventListener("click", (event) => editar(event));
+btLimpar.addEventListener("click", (event) => {
+    event.preventDefault();
+    limpar(event);
+});
 
 function cadastrar(event) {
     event.preventDefault();
@@ -21,7 +30,6 @@ function cadastrar(event) {
         incluso: inIncluso.value,
         negociacao: rdNegociacao.value,
         cidade: rdCidade.value,
-
     }
     if (inNumero.value == "" || inPreco.value == "" || inComodos.value == "" || inIncluso.value == "") {
         if (inNumero.value == "") {
@@ -50,7 +58,6 @@ function cadastrar(event) {
                 outSaida.textContent = `Apartamento número ${cadastroAp.numero} já cadastrado`;
                 flag = false;
             }
-
         }
         if (flag) {
             vetApartamentos.push(cadastroAp);
@@ -75,46 +82,82 @@ function deletar(event) {
             }
         }
         console.log(vetApartamentos);
-        if(flag){
+        if (flag) {
             outSaida.innerHTML = `O apartamento numero ${numero} não foi encontrado`
             inNumero.focus();
-        }else{
+        } else {
             outSaida.innerHTML = `O apartemento numero ${numero} foi deletado com sucesso!`
             localStorage.setItem("vetApartamentos", JSON.stringify(vetApartamentos));
         }
     }
 
 }
-function editar (event){
+function editar(event) {
     event.preventDefault();
     let vetApartamentos = JSON.parse(localStorage.getItem("vetApartamentos"));
     let numero = Number(inNumero.value)
-    if(inNumero.value == ""){
+    if (inNumero.value == "") {
         outSaida.innerHTML = "Para editar um apartamento é necessario informar o numero do apartamento"
         inNumero.focus();
-    }else{
+    } else {
         let flag = true;
-        let ap ;
-        for(let ind = 0; ind < vetApartamentos.length; ind++){
-            if(vetApartamentos[ind].numero == numero){
+        let ap;
+        for (let ind = 0; ind < vetApartamentos.length; ind++) {
+            if (vetApartamentos[ind].numero == numero) {
                 ap = vetApartamentos[ind]
                 flag = false;
             }
         }
-        if(flag){
+        if (flag) {
             outSaida.innerHTML = `O apartamneto numero ${numero} não foi encontrado!`
-            inNumero.focus(); 
-        }else{
-            inNumero.disabled = true;
-            //campos de input 
-            inPreco.value = ap.valor;
-            inComodos.value = ap.comodos;
-            inIncluso.value = ap.incluso;
-            //campos radio button 
-            ap.negociacao == "venda" ? rdNegociacao[0].checked = true : rdNegociacao[1].checked = true;
-            ap.cidade == "Santa Teresa" ? rdCidade[0].checked = true : rdCidade[1].checked = true;
+            inNumero.focus();
+        } else {
+            if (btEditar.innerHTML == "Editar Apartamento") {
+                inNumero.disabled = true;
+                //campos de input 
+                inPreco.value = ap.valor;
+                inComodos.value = ap.comodos;
+                inIncluso.value = ap.incluso;
+                //campos radio button 
+                ap.negociacao == "venda" ? rdNegociacao[0].checked = true : rdNegociacao[1].checked = true;
+                ap.cidade == "Santa Teresa" ? rdCidade[0].checked = true : rdCidade[1].checked = true;
+                btLimpar.disabled = true;
 
+                btEditar.innerHTML = "Confirmar edição"
+            } else {
+                let edicao = {
+                    numero: Number(inNumero.value),
+                    valor: Number(inPreco.value),
+                    comodos: inComodos.value,
+                    incluso: inIncluso.value,
+                    negociacao: rdNegociacao.value,
+                    cidade: rdCidade.value,
+                }
+                for (let ind = 0; ind < vetApartamentos.length; ind++) {
+                    if(vetApartamentos[ind].numero == numero){
+                        vetApartamentos[ind] = edicao;
+                    }
+                }
+                localStorage.setItem("vetApartamentos", JSON.stringify(vetApartamentos));
+                outSaida.innerHTML = `O apartamento numero ${numero} foi editado com sucesso!`
+                btEditar.innerHTML ="Editar Apartamento";
+                inNumero.disabled = false;
+                btLimpar.disabled = false;
+                limpar();
+            }
         }
     }
 }
+function limpar(event){
+    // event.preventDefault();
+    inNumero.value = "";
+    rdCidade[0].checked = false;
+    rdCidade[1].checked = false;
+    rdNegociacao[0].checked = false;
+    rdNegociacao[1].checked = false;
+    inPreco.value = "";
+    inIncluso.value = "";
+    inComodos.value = "";
+    inNumero.disabled = false;
 
+}
